c24ada71b5cd43995e55435f623d118ff45b45f6
tsuchiwww <ylwrvr.t.koga@gmail.com>
2020-08-28 02:48:55 +0900

    [AC] abc166 abc166_d

---
+++ atcoder.jp/abc166/abc166_d/Main.py
@@ -0,0 +1,9 @@
+X = int(input())
+for a in range(-1000, 1000):
+    for b in range(-1000, 1000):
+        if (a ** 5) - (b ** 5) == X:
+            print(a, b)
+            break
+    else:
+        continue
+    break
