3b70309944206c1fa86c94e9c6348dbc31e314de
tsuchiwww <ylwrvr.t.koga+git@gmail.com>
2020-09-03 02:41:28 +0900

    [AC] APG4b APG4b_cl

---
+++ atcoder.jp/APG4b/APG4b_cl/Main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int N, A;
+    cin >> N;
+    cin >> A;
+    for (int i = 0; i < N; i ++){
+        string op;
+        int B;
+        cin >> op >> B;
+        if (op == "+"){
+            A += B;
+        }
+        else if (op == "-"){
+            A -= B;
+        }
+        else if (op == "*"){
+            A *= B;
+        }
+        else if (op == "/"){
+            if (B == 0){
+                cout << "error" << endl;
+                break;
+            }
+            else{
+                A /= B;
+            }
+        }
+        cout << i + 1 << ":" << A << endl;
+    }
+}
