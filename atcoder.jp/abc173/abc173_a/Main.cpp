2959d54e7cf8c16609b896fcbf2181598cad7437
tsuchiwww <ylwrvr.t.koga+git@gmail.com>
2020-09-20 23:43:15 +0900

    [AC] abc173 abc173_a

---
+++ atcoder.jp/abc173/abc173_a/Main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n; cin >> n;
+  if(n % 1000 == 0){
+  	cout << 0 << endl;
+    return 0;
+  }
+  cout << 1000 - (n % 1000) << endl;
+}
