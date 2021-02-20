864e316d900378fbda16fce0cc7932c948f2136c
tsuchiwww <ylwrvr.t.koga+git@gmail.com>
2021-02-20 21:06:50 +0900

    [AC] abc192 abc192_a

---
+++ atcoder.jp/abc192/abc192_a/Main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i ++)
+
+vector <int> dy = {0, 1, 0, -1};
+vector <int> dx = {1, 0, -1, 0};
+
+const int INF = 1000000000;
+const ll INFLL = 100000000000000000;
+
+int main(){
+    int x; cin >> x;
+    int c = 1;
+    while((x + c) % 100 != 0){
+        c++;
+    }
+    cout << c << endl;
+}
