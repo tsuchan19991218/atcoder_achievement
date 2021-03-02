e4dd43c204db0e2d199eb710e5fd4a96644949f0
tsuchiwww <ylwrvr.t.koga+git@gmail.com>
2021-03-03 02:09:47 +0900

    [AC] abc180 abc180_c

---
+++ atcoder.jp/abc180/abc180_c/Main.cpp
@@ -0,0 +1,86 @@
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i ++)
+
+vector <int> dy = {0, 1, 0, -1};
+vector <int> dx = {1, 0, -1, 0};
+
+const int INF = 1000000000;
+const ll INFLL = 100000000000000000;
+
+vector <ll> get_divisor(ll n){
+    vector <ll> ans;
+    for(ll i = 1; i * i <= n; i ++){
+        if(n % i == 0){
+            ans.push_back(i);
+            if(i * i == n) continue;
+            ans.push_back(n / i);
+        }
+    }
+    sort(ans.begin(), ans.end());
+    return ans;
+}
+
+int main(void){
+    // Your code here!
+    ll n;
+    cin >> n;
+    auto vec = get_divisor(n);
+    for(auto num: vec) cout << num << endl;
+}
