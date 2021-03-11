d889b57e93f0ccc23b1bd8ffc16232606c021156
tsuchiwww <ylwrvr.t.koga+git@gmail.com>
2021-03-11 21:01:36 +0900

    [AC] abc117 abc117_a

---
+++ atcoder.jp/abc117/abc117_a/Main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i ++)
+
+vector <int> dy = {0, 1, 0, -1};
+vector <int> dx = {1, 0, -1, 0};
+
+const int INF = 1000000000;
+const ll INFLL = 100000000000000000;
+
+int main(){
+    double t, x; cin >> t >> x;
+    cout << t / x << endl;
+}
