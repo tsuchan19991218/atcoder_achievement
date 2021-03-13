ea6a6e5c24983cb3aa4cf211ee022d2dbcbfe46e
tsuchiwww <ylwrvr.t.koga+git@gmail.com>
2021-03-13 21:01:04 +0900

    [AC] abc195 abc195_a

---
+++ atcoder.jp/abc195/abc195_a/Main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i ++)
+
+vector <int> dy = {0, 1, 0, -1};
+vector <int> dx = {1, 0, -1, 0};
+
+const int INF = 1000000000;
+const ll INFLL = 100000000000000000;
+
+int main(){
+    int m, h; cin >> m >> h;
+    if(h % m == 0) cout << "Yes" << endl;
+    else cout << "No" << endl;
+}
