f23416092f75e6ee5ad5cecd4d78ffd0df87f77f
tsuchiwww <ylwrvr.t.koga+git@gmail.com>
2021-05-08 21:03:37 +0900

    [AC] abc200 abc200_b

---
+++ atcoder.jp/abc200/abc200_b/Main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <math.h>
+using namespace std;
+typedef long long ll;
+#define rep(i, n) for(int i = 0; i < (int)(n); i ++)
+
+vector <int> dy = {0, 1, 0, -1};
+vector <int> dx = {1, 0, -1, 0};
+
+const int INF = 1000000000;
+const ll INFLL = 100000000000000000;
+
+long long pow(long long x, long long n) {
+    long long ret = 1;
+    while (n > 0) {
+        if (n & 1) ret *= x;  // n の最下位bitが 1 ならば x^(2^i) をかける
+        x *= x;
+        n >>= 1;  // n を1bit 左にずらす
+    }
+    return ret;
+}
+
+int main(){
+	ll n; cin >> n;
+	int k; cin >> k;
+	rep(i, k){
+		if(n % 200 == 0) n /= 200;
+		else{
+			n *= 1000;
+			n += 200;
+		}
+	}
+	cout << n << endl;
+}
